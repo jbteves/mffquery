58e2fee6b22356d642147fbb114de0d2ad54f5b4
Joshua Teves <joshua.teves@nih.gov>
2022-08-19 17:25:03 -0400

    Total rework

--- mffquery.py
+++ mffquery.py
@@ -6,6 +6,9 @@ from datetime import datetime
 import mffpy
 
 
+VALID_COLUMNS = ('code', 'description', 'label', 'none')
+
+
 def str_to_time(t: str) -> datetime:
     # Shave off UTC offset
     t = t[:-6]
@@ -21,54 +24,81 @@ def dt_to_seconds(t: str, start) -> str:
 
 def main():
     parser = ArgumentParser(description='.mff querying tool')
-    parser.add_argument('file', help='The file to query.')
     parser.add_argument(
-        '--time',
+        '--clock',
         help=(
-            "Use datetime instead of seconds from recording start "
-            "for event onsets."
+            "Use clock time instead of time relative to record start."
         ),
         action='store_true',
     )
+    parser.add_argument(
+        '--number',
+        help=(
+            "Number events as the leftmost column."
+        ),
+        action='store_true'
+    )
+#    parser.add_argument(
+#        '--csv',
+#        help=(
+#            "Store output in csv file instead of print to standard out."
+#        )
+#    )
+    parser.add_argument(
+        'file',
+        help=".mff file to read events from.",
+    )
+    parser.add_argument(
+        'column',
+        help=(
+            "Additional columns to print. "
+            f"Valid values are {VALID_COLUMNS}."
+        ),
+        nargs='+',
+    )
     args = parser.parse_args()
+    for c in args.column:
+        if c not in VALID_COLUMNS:
+            raise ValueError(
+                f"{c} is not a valid column type. "
+                f"Valid column types are: {VALID_COLUMNS}"
+            )
+    columns_to_add = args.column
+    if 'none' in columns_to_add and len(columns_to_add) != 1:
+        raise ValueError(
+            f"Cannot combine 'none' and other columns {columns_to_add}"
+        )
+    elif 'none' in columns_to_add:
+        add_columns = False
+    else:
+        add_columns = True
     info = mffpy.Reader(args.file)
+    is_relative = not args.clock
+    add_numbers = args.number
+#    use_stdout = not args.csv
+
     mff_content = info.get_mff_content()
     start_time = str_to_time(mff_content['fileInfo']['recordTime'])
-    print(start_time)
     event_list = mff_content['eventTrack']['event']
     count = 0
-    LABEL = "Label"
-    if args.time:
-        START = "Start Time"
-    else:
-        START = "Start Time (s)"
-    DURATION = "Dur (ms)"
-    if args.time:
-        header_line = f"N   \tCODE\t{LABEL:10}\t{START:25}\t\t{DURATION:<6}"
-    else:
-        header_line = f"N   \tCODE\t{LABEL:10}\t{START:15}\t{DURATION:<6}"
-    dash_line = '-'*80
-    print(header_line)
-    print(dash_line)
     for event in event_list:
-        label = event['label']
-        code = event['code']
-        description = event['description']
-        if args.time:
-            begin_time = event['beginTime']
-        else:
-            begin_time = dt_to_seconds(event['beginTime'], start_time)
-        duration = event['duration']
-        if args.time:
-            print(
-                f"{count:04}\t{code:4}\t{label:10}\t{begin_time:25}\t{duration:<6}"
-            )
-        else:
-            print(
-                f"{count:04}\t{code:4}\t{label:10}\t{begin_time:15}\t{duration:<6}"
-            )
-
+        to_print = []
+        if add_numbers:
+            to_print.append(str(count))
         count += 1
+        # get a time string
+        time_str = event['beginTime']
+        if is_relative:
+            event_time = str_to_time(event['beginTime'])
+            difference = event_time - start_time
+            time = f"{difference.seconds:04}.{difference.microseconds:06}"
+        else:
+            time = time_str
+        to_print.append(time)
+        if add_columns:
+            for col in columns_to_add:
+                to_print.append(event[col])
+        print('\t'.join(to_print))
 
 
 if __name__ == '__main__':
